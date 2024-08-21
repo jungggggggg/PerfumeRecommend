31bf08a33d12615981226c55834c2b1da53a6420
jungggggggg <jungsu0705@naver.com>
2024-08-21 16:52:03 +0900

    web error again

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-// webpack.config.js
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-
-module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
-  // 필요한 경우 웹팩 설정을 커스터마이징하세요
-  return config;
-};
