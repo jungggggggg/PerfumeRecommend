c22999a59c7eb0eabcdfa851873905bf573fd8c1
jungggggggg <jungsu0705@naver.com>
2024-08-21 16:35:21 +0900

    web error

--- index.web.js
+++
@@ -1,9 +0,0 @@
-// index.web.js
-import 'expo-router/entry'; // Expo Router의 진입점을 가져옵니다
-import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
-
-AppRegistry.runApplication(appName, {
-  initialProps: {},
-  rootTag: document.getElementById('app-root'),
-});
